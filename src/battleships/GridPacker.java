1774bff5b4e63a07f088bbb8b2ef5628f0ecd20a
Winston Berty <w.berty@gmail.com>
2015-05-03 20:29:36 +0100

    inference stuff. About to get deleted to focus on probability approach

--- src/battleships/GridPacker.java
+++ src/battleships/GridPacker.java
@@ -90,13 +90,17 @@ public class GridPacker
             int startY = Coordinates.minY( (Coordinates[]) shipCases.toArray() );
             // get max, to end at
             int endY = Coordinates.maxY( (Coordinates[]) shipCases.toArray() );
-            for (int y = startY + 1; y < endY; y++) {
-                // make the probabilitycase
-                ProbabilityCase newShipCase = new ProbabilityCase(contents, 1);
-                // add it to the grid at the relevent coordinates
-                grid.setCase(x, y, newShipCase);
-                // add it to the grid's list of known ship cases
-                grid.addKnownCase(type, new Coordinates(x,y));
+            // iterate over middle cases and add cases where don't yet exist
+            for (int y = startY; y <= endY; y++) {
+                // check for existance
+                if (grid.cases[x][y].getContents() == CaseContents.UNKNOWN) {
+                    // make the probabilitycase
+                    ProbabilityCase newShipCase = new ProbabilityCase(contents, 1);
+                    // add it to the grid at the relevent coordinates
+                    grid.setCase(x, y, newShipCase);
+                    // add it to the grid's list of known ship cases
+                    grid.addKnownCase(type, new Coordinates(x,y));
+                }
             }
             return true;
         }
@@ -107,13 +111,17 @@ public class GridPacker
             int startX = Coordinates.minX( (Coordinates[]) shipCases.toArray() );
             // get max, to end at
             int endX = Coordinates.maxX( (Coordinates[]) shipCases.toArray() );
-            for (int x = startX + 1; x < endX; x++) {
-                // make the probabilitycase
-                ProbabilityCase newShipCase = new ProbabilityCase(contents, 1);
-                // add it to the grid at the relevent coordinates
-                grid.setCase(x, y, newShipCase);
-                // add it to the grid's list of known ship cases
-                grid.addKnownCase(type, new Coordinates(x,y));
+            // iterate over middle cases and add cases where don't yet exist
+            for (int x = startX; x <= endX; x++) {
+                // check for existance and add only if doesn't exist yet
+                if (grid.cases[x][y].getContents() == CaseContents.UNKNOWN) {
+                    // make the probabilitycase
+                    ProbabilityCase newShipCase = new ProbabilityCase(contents, 1);
+                    // add it to the grid at the relevent coordinates
+                    grid.setCase(x, y, newShipCase);
+                    // add it to the grid's list of known ship cases
+                    grid.addKnownCase(type, new Coordinates(x,y));
+                }
             }
             return true;
         }
@@ -128,25 +136,14 @@ public class GridPacker
             // check case at maxY + 1
             int endY = Coordinates.maxY( (Coordinates[]) shipCases.toArray() );
             if (startY == 0 || grid.cases[x][startY - 1].getContents() != CaseContents.UNKNOWN) {
-                // fill in cases from the start case to the end
-                setCasesBetween(shipCases, type, grid);
-                // then from the end to the full length of the ship type
-                ArrayList<Coordinates> endCases = new ArrayList<>();
-                endCases.add(new Coordinates(x, endY));
-                // the end coordinate must be one over to give a correct fill
-                endCases.add(new Coordinates(x, startY + type.getLength() ));
-                return setCasesBetween(endCases, type, grid);
+                // add new calculated end case            
+                shipCases.add(new Coordinates(x, endY));
             }
             else if (endY == grid.getWidth() - 1 || grid.cases[x][endY + 1].getContents() != CaseContents.UNKNOWN) {
-                // fill in cases from the start case to the end
-                setCasesBetween(shipCases, type, grid);
-                // then backwards from the start to make the full length of the ship type
-                ArrayList<Coordinates> beforeCases = new ArrayList<>();
-                beforeCases.add(new Coordinates(x, startY));
-                beforeCases.add(new Coordinates(x, endY - type.getLength() ));
-                return setCasesBetween(beforeCases, type, grid);
+                // add new calculated start case  
+                shipCases.add(new Coordinates(x, endY - type.getLength() + 1 )); // + 1 as 0-indexed
             }
-            return false;
+            return setCasesBetween(shipCases, type, grid);
         }
         else if (Coordinates.alignedY( (Coordinates[]) shipCases.toArray() )) {
             int y = shipCases.get(0).getY();
@@ -154,26 +151,15 @@ public class GridPacker
             int startX = Coordinates.minX( (Coordinates[]) shipCases.toArray() );
             // check case at maxX + 1
             int endX = Coordinates.maxX( (Coordinates[]) shipCases.toArray() );
-            if (startX == 0 || grid.cases[startX-1][y].getContents() != CaseContents.UNKNOWN) {
-                // fill in cases from the start case to the end
-                setCasesBetween(shipCases, type, grid);
-                // then from the end to the full length of the ship type
-                ArrayList<Coordinates> endCases = new ArrayList<>();
-                endCases.add(new Coordinates(endX, y));
-                // the end coordinate must be one over to give a correct fill
-                endCases.add(new Coordinates(startX + type.getLength(),y));
-                return setCasesBetween(endCases, type, grid);
+            if (startX == 0 || grid.cases[startX - 1][y].getContents() != CaseContents.UNKNOWN) {
+                // add new calculated end case            
+                shipCases.add(new Coordinates(endX, y));
             }
             else if (endX == grid.getHeight() - 1 || grid.cases[endX + 1][y].getContents() != CaseContents.UNKNOWN) {
-                // fill in cases from the start case to the end
-                setCasesBetween(shipCases, type, grid);
-                // then backwards from the start to make the full length of the ship type
-                ArrayList<Coordinates> beforeCases = new ArrayList<>();
-                beforeCases.add(new Coordinates(startX, y));
-                beforeCases.add(new Coordinates(endX - type.getLength(), y ));
-                return setCasesBetween(beforeCases, type, grid);
+                // add new calculated start case  
+                shipCases.add(new Coordinates(endX - type.getLength() + 1  , y)); // + 1 as 0-indexed
             }
-            return false;
+            return setCasesBetween(shipCases, type, grid);
         }
         return false;
     }
@@ -213,6 +199,90 @@ public class GridPacker
         return false;
     }
     
+    boolean fillInCaged(ArrayList<Coordinates> shipCases, ShipType type, ProbabilityGrid grid) {
+        Coordinates knownCase = shipCases.get(0);
+        int x = knownCase.getX();
+        int y = knownCase.getY();
+        // check on which axis the case has 2 blocking neighbours
+        boolean blockedNorth =  (x == 0 || grid.cases[x-1][y].getContents() != CaseContents.UNKNOWN);
+        boolean blockedSouth =  (x == grid.getHeight() - 1 || grid.cases[x+1][y].getContents() != CaseContents.UNKNOWN);
+        boolean blockedWest =  (y == 0 || grid.cases[x][y-1].getContents() != CaseContents.UNKNOWN);
+        boolean blockedEast =  (y == grid.getWidth() - 1 || grid.cases[x][y+1].getContents() != CaseContents.UNKNOWN);
+        // make an arraylist we will use to fill in cases assuming the method finds a ship
+        ArrayList<Coordinates> bounds = new ArrayList<>();
+        if (blockedNorth && blockedSouth) {
+            // go forward in the y direction to a max distance equalling the
+            // ships length, checking for a blocking case. Count the number of 
+            // cases gone forward
+            int casesForward = 0;
+            for (int v = y; v < y + type.getLength(); v++) {
+                if (v < grid.getWidth() - 1 || grid.cases[x][v+1].getContents() == CaseContents.UNKNOWN) {
+                    casesForward++;
+                }
+                else break;
+            }
+            // now go backwards...
+            int casesBackward = 0;
+            for (int v = y; v > y - type.getLength(); v--) {
+                if (v > 0 || grid.cases[x][v-1].getContents() == CaseContents.UNKNOWN) {
+                    casesBackward++;
+                }
+                else break;
+            }
+            // check the sum
+            if (casesBackward + casesForward + 1 == type.getLength()) {
+                // exactly right length, therefore ship found
+                // add start and end case to a new ArrayList we will use to construct
+                bounds.add(new Coordinates(x,y - casesBackward));
+                bounds.add(new Coordinates(x,y + casesForward));
+                return setCasesBetween(bounds, type, grid);
+            }
+            return false;
+        }
+        if (blockedWest && blockedEast) {
+            // go forward in the x direction to a max distance equalling the
+            // ships length, checking for a blocking case. Count the number of 
+            // cases gone forward
+            int casesForward = 0;
+            for (int u = x; u < x + type.getLength(); u++) {
+                if (u < grid.getHeight() - 1 || grid.cases[u+1][y].getContents() == CaseContents.UNKNOWN) {
+                    casesForward++;
+                }
+                else break;
+            }
+            // same for reverse direction
+            int casesBackward = 0;
+            for (int u = x; u > x - type.getLength(); u--) {
+                if (u > 0 || grid.cases[u-1][y].getContents() == CaseContents.UNKNOWN) {
+                    casesBackward++;
+                }
+                else break;
+            }
+            // check the sum
+            if (casesBackward + casesForward + 1 == type.getLength()) {
+                // exactly right length, therefore ship found
+                // add start and end case to a new ArrayList we will use to construct
+                bounds.add(new Coordinates(x - casesBackward,y));
+                bounds.add(new Coordinates(x + casesForward,y));
+                return setCasesBetween(bounds, type, grid);
+            }
+            return false;
+        }
+        return false;
+    }
+    
+    ArrayList<Coordinates>[] discernibleCruisers(ArrayList<Coordinates> shipCases) {
+        // We can only be sure 2 cruiser cases belong to separate ships if they
+        // have at least distance 3 between them on the same axis
+        // We must therefore split the cases in the ArrayList into 2 ArrayLists
+        ArrayList<Coordinates> cruiser1 = new ArrayList<>();
+        ArrayList<Coordinates> cruiser2 = new ArrayList<>();
+        // start with the first Coordinates element in cruiser1
+        cruiser1.add(shipCases.get(0));
+        // now we iterate over the rest and add to the first only if distance
+        // is greater than or equal to 3
+    }
+    
     boolean inferForType(ShipType type, ArrayList<Coordinates> shipCases) {
         /**
          * we can infer the remaining cases  of a ship if: we know the 2
@@ -222,11 +292,10 @@ public class GridPacker
          * and by cases on the other axis at a distance apart equal to the
          * length of the ship.
          * The exception to this is for cruisers, which don't obey the same
-         * logic as there are 2 of them on the grid. Deal with these separately.
+         * logic as there are 2 of them on the grid, UNLESS we can be sure the 2
+         * are a certain distance apart. Thus for cruisers we call this method
+         * twice, once with each separated set of cases for each discernible cruiser
          */
-        if (type == ShipType.CRUISER) {
-            return inferForCruisers(shipCases);
-        }
         int typeLen = type.getLength();
         int numKnown = shipCases.size();
         // check if all the cases have already been found
@@ -244,9 +313,10 @@ public class GridPacker
             else if (fillInFreeDirection(shipCases, type, inferredConfiguration)) {
                 anyInferred = true;
             }
-            // case where 
-            else {
-            
+            // case where one point is know but may be caged on two same-axis
+            // sides and at a distance equal to the ships lenght on the other axis
+            else if (fillInCaged(shipCases, type, inferredConfiguration)) {
+                anyInferred = true;
             }
             return anyInferred;
         }
